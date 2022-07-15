3f060eeddb2ed20dc766a137bb7bf98a96c4440a
Ziad <zmali002@fiu.edu>
2022-07-15 12:37:23 -0500

    Setting up Repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Graph from './components/Graph/Graph';
+
+function App() {
+  return (
+    <div className="App">
+      <Graph/>
+    </div>
+  );
+}
+
+export default App;
